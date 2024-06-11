24393372a0468c2d6e173545d47f0a4664da3570
kysclient <kysclient@gmail.com>
2024-06-11 09:25:37 +0900

    feat:init

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className='bg-[#58C4Dc] text-white p-3 mb-5'>
+      <h1 className='text-2xl text-center font-bold uppercase'>
+        React 19 Playground
+      </h1>
+    </header>
+  );
+};
+export default Header;
